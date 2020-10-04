9df104adee647ec1da8032e0002a57d0bcdb41eb
isboston <itvibn@mail.ru>
2020-10-04 21:05:39 +0300

    task_4_1.py

---
+++ task_4_1.py
@@ -0,0 +1,9 @@
+li = [1, -2, 3, 4, 5]
+for li_item in li:
+    new_li = li_item * -2
+    print(new_li)
+count = 0
+while count < len(li):
+    new_li = (li[count]) * -2
+    print(new_li)
+    count += 1
