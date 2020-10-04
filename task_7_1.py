679361a6e30058c9cd4d1c9550aba3e6944e740a
isboston <itvibn@mail.ru>
2020-10-04 22:40:36 +0300

    task_7_1.py

---
+++ task_7_1.py
@@ -0,0 +1,106 @@
+def inch_to_cm(inch):
+    convert_inch_to_cm = inch * 2.54
+    print(convert_inch_to_cm)
+    return convert_inch_to_cm
+
+
+inch_to_cm(float(input()))
+
+
+def cm_to_inch(cm):
+    convert_cm_to_inch = cm * 0.39
+    print(convert_cm_to_inch)
+    return convert_cm_to_inch
+
+
+cm_to_inch(float(input()))
+
+
+def mile_to_km(mile):
+    convert_mile_to_km = mile * 1.61
+    print(convert_mile_to_km)
+    return convert_mile_to_km
+
+
+mile_to_km(float(input()))
+
+
+def km_to_mile(km):
+    convert_km_to_mile = km * 0.62
+    print(convert_km_to_mile)
+    return convert_km_to_mile
+
+
+km_to_mile(float(input()))
+
+
+def pound_to_kg(pound):
+    convert_pound_to_kg = pound * 0.45
+    print(convert_pound_to_kg)
+    return convert_pound_to_kg
+
+
+pound_to_kg(float(input()))
+
+
+def kg_to_pound(kg):
+    convert_kg_to_pound = kg * 2.2
+    print(convert_kg_to_pound)
+    return convert_kg_to_pound
+
+
+kg_to_pound(float(input()))
+
+
+def ounces_to_grams(ounces):
+    convert_ounces_to_grams = ounces * 28.35
+    print(convert_ounces_to_grams)
+    return convert_ounces_to_grams
+
+
+ounces_to_grams(float(input()))
+
+
+def grams_to_ounces(grams):
+    convert_grams_to_ounces = grams * 0.035
+    print(convert_grams_to_ounces)
+    return convert_grams_to_ounces
+
+
+grams_to_ounces(float(input()))
+
+
+def gallon_in_liters(gallon):
+    convert_gallon_in_liters = gallon * 4.55
+    print(convert_gallon_in_liters)
+    return convert_gallon_in_liters
+
+
+gallon_in_liters(float(input()))
+
+
+def liters_in_gallon(liters):
+    convert_liters_in_gallon = liters * 0.22
+    print(convert_liters_in_gallon)
+    return convert_liters_in_gallon
+
+
+liters_in_gallon(float(input()))
+
+
+def pints_in_liters(pints):
+    convert_pints_in_liters = pints * 0.57
+    print(convert_pints_in_liters)
+    return convert_pints_in_liters
+
+
+pints_in_liters(float(input()))
+
+
+def liters_in_pints(liters):
+    convert_liters_in_pints = liters * 1.76
+    print(convert_liters_in_pints)
+    return convert_liters_in_pints
+
+
+liters_in_pints(float(input()))
