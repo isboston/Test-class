3141dc2963f32b1708b8d7c5c1625d5a30f927d5
isboston <itvibn@mail.ru>
2020-11-26 21:36:55 +0300

    correct the error

--- task_10_2.py
+++ task_10_2.py
@@ -14,28 +14,30 @@ rows = [
     [date(2020, 11, 25), 'Brest', '2', '14'],
     [date(2020, 11, 25), 'Minsk', '3', '10'],
 ]
-with open('weather.csv', 'w') as f:
-    csvwriter = csv.writer(f)
+with open('weather.csv', 'w') as weather:
+    csvwriter = csv.writer(weather)
     csvwriter.writerow(fields)
     csvwriter.writerows(rows)
 
-with open('weather.csv', 'r') as f:
-    delta = timedelta(7)
-    weather = [0, 0]
+with open('weather.csv', 'r') as weather:
+    number_of_days = int(input('Enter the number of days:\n'))
+    delta = timedelta(number_of_days)
+    weather_temp = 0
+    weather_speed = 0
     count = 0
     lines = []
-    csvreader = csv.reader(f)
+    csvreader = csv.reader(weather)
     fields = next(csvreader)
     for line in csvreader:
         lines.append(line)
 
     for line in lines:
-        a = datetime.strptime(line[0], '%Y-%m-%d').date()
-        if line[1] == 'Minsk' and date.today() - a <= delta:
-            weather[0] += int(line[2])
-            weather[1] += int(line[3])
+        dt = datetime.strptime(line[0], '%Y-%m-%d').date()
+        if line[1] == 'Minsk' and date.today() - dt <= delta:
+            weather_temp += int(line[2])
+            weather_speed += int(line[3])
             count += 1
-    avg_temp = weather[0] / count
-    avg_speed = weather[1] / count
+    avg_temp = weather_temp / count
+    avg_speed = weather_speed / count
     print(avg_temp)
     print(avg_speed)
